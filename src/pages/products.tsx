e350d625ca6aa84f35e814d7ad1639f2fc784716
nickseth <nickseth71@gmail.com>
2022-12-29 17:31:49 +0530

    bhupendra 29 dec

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
-import AllProducts from '../components/all-products/all-products';
+import { StaticQuery, graphql } from 'gatsby';
+import ProductGrid from './../components/product-grid/product-grid';
 
-const ProductsPage = () => {
-    return (
-        <AllProducts />
-    );
-}
+const AllProductStaticQuery = graphql`
+	query {
+		allShopifyProduct {
+			edges {
+				node {
+					id
+					title
+					handle
+					createdAt
+					shopifyId
+					availableForSale
+					variants {
+						id
+						price
+						priceV2 {
+							amount
+							currencyCode
+						}
+						shopifyId
+						availableForSale
+					}
+					images {
+						id
+						originalSrc
+						localFile {
+							childImageSharp {
+								fluid {
+									...GatsbyImageSharpFluid_withWebp_tracedSVG
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 
+const ProductsPage: React.FC<{ withLink?: boolean }> = ({
+    withLink = true,
+}) => (
+    <StaticQuery<GatsbyTypes.Query>
+        query={`${AllProductStaticQuery}`}
+        render={({ allShopifyProduct }) => {
+            const products = allShopifyProduct?.edges;
+            return (
+                <ProductGrid
+                    id="feature"
+                    withLink={withLink}
+                    gridTitle="All Products"
+                    products={products}
+                />
+            );
+        }}
+    />
+);
 
 export default ProductsPage;
