0c9aefe9fc501bedee7b32db1c28a8bb317d8be7
nickseth <nickseth71@gmail.com>
2022-12-29 17:05:46 +0530

    bhupendra 29 dec

---
+++ src/pages/products.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AllProducts from '../components/all-products/all-products';
+
+const ProductsPage = () => {
+    return (
+        <AllProducts />
+    );
+}
+
+
+export default ProductsPage;
