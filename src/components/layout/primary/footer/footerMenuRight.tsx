c797561e378fca294e092b334031aa01856ce2aa
VarunmehraRG <varun.mehra@razor-group.com>
2023-01-02 11:47:11 +0530

    soumya 2 jan

--- src/components/layout/primary/footer/footerMenuRight.tsx
+++ src/components/layout/primary/footer/footerMenuRight.tsx
@@ -1,63 +1,68 @@
-// /** @jsx jsx */
-// import { Link, StaticQuery, graphql } from 'gatsby';
-// import { Box, Text, jsx } from 'theme-ui';
+/** @jsx jsx */
+import { Link, StaticQuery, graphql } from 'gatsby';
+import { Box, Text, jsx } from 'theme-ui';
 
-// const footerStaticQuery = graphql`
-// 	query {
-// 		prismic {
-// 			allFooters {
-// 				edges {
-// 					node {
-//                         menu_title 
-//                         menu_group {
-//                             menu_image1_link{
-//                                     ... on PRISMIC__ExternalLink {
-// 									url
-// 								    }
-//                                 }
-//                                 menu_image_1
-//                             }
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// `;
+const footerStaticQuery = graphql`
+	query {
+		prismic {
+			allFooters {
+				edges {
+					node {
+                        menu_title 
+                        menu_group {
+                            menu_image1_link{
+                                    ... on PRISMIC__ExternalLink {
+									url
+								    }
+                                }
+                                menu_image_1
+                            }
+					}
+				}
+			}
+		}
+	}
+`;
 
-// const getTitleData = (links) => {
-//     const data = [];
-//     links.forEach((link) => {
-//         data.push({ type: link.menu_image_1, link: link.menu_image1_link.url });
-//     });
-//     return data;
-// };
+const getTitleData = (links) => {
+    const data = [];
+    links.forEach((link) => {
+        data.push({ type: link.menu_image_1, link: link.menu_image1_link.url });
+    });
+    return data;
+};
 
-// const FooterMenu: React.FC<{ fluid?: boolean }> = ({ fluid }) => (
-//     <StaticQuery<GatsbyTypes.Query>
+ const footerMenuRight: React.FC<{ fluid?: boolean }> = ({ fluid }) => (
+   <StaticQuery<GatsbyTypes.Query>
 //         query={`${footerStaticQuery}`}
 //         render={(data) => {
 //             console.log("product data ===========>",data.prismic.allFooters.edges);
-//             return (
-//                 <div>
-//                     {
-//                         data.prismic.allFooters.edges?.map((item: any, i: any) => {
-//                             return (
-//                                 <div key={i}>
-//                                     <h4>{item.node.menu_heading[0].text}</h4>
-//                                 {item.node.footer_menu.map((item2:any,index1:any)=>{
-//                                     return (
-//                                         <Link to={getTitleData(item.node.footer_menu).link}><p key={index1}>{item2.title[0].text}</p></Link>
-//                                     )
-//                                     })
-//                                    }
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//             )
+//             // return (
+//             //     <div>
+//             //         {
+//             //             data.prismic.allFooters.edges?.map((item: any, i: any) => {
+//             //                 return (
+//             //                     <div key={i}>
+//             //                         <h4>{item.node.menu_title[0].text}</h4>
+//             //                     {item.node.menu_group.map((item2:any,index1:any)=>{
+//             //                         console.log(item2);
+//             //                         // return (
+//             //                         //      <Link to={getTitleData(item.node.menu_group).link}>
+//             //                         //         <img src={item2.menu_image_1[0].image} />
+                                            
+//             //                         //         {/* <p key={index1}>{item2.title[0].text}</p> */}
+//             //                         //     </Link>
+//             //                         // )
+//             //                         })
+//             //                        }
+//             //                     </div>
+//             //                 )
+//             //             })
+//             //         }
+//             //     </div>
+//             // )
 //         }}
-//     />
-// );
+    />
+ );
 
-// export default FooterMenu;
+export default footerMenuRight;
