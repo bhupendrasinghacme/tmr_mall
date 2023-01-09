4239eb39bcb4467a868e1775d8c03cabe5da81c4
nickseth <nickseth71@gmail.com>
2023-01-09 16:52:16 +0530

    bhupendra key error resolve

--- src/components/textWithLink/textwithLink.tsx
+++ src/components/textWithLink/textwithLink.tsx
@@ -41,44 +41,44 @@ const TextWithLink = () => (
         render={(data: any) => {
             return (
                 <div className='textwithLink-wrapper'>
-                        {
-                            data.prismic.allHomes.edges?.map((item: any, i: any) => {
-                                return (
-                                    <div key={i} className='textwithLink-group'>
-                                        <h1 className="title">{item.node.top_offers_title[0].text}</h1>
-                                       <div className='twl-item-wrap'>
-                                          {item.node.top_offers_group.map((item2: any, index1: any) => {
-                                              return (
-                                                    <div className='content_wrap' key={"links_"+index1}>
-                                                       {item2.heading[0].text &&
-                                                        <div className='heading'>{item2.heading[0].text}</div>
-                                                        }
-                                                        {item2.sub_heading[0].text &&
-                                                        <div className='sub_heading'>{item2.sub_heading[0].text}</div>
-                                                        }
-                                                        {item2.view_offers_text[0].text &&
-                                                        <Link to={getTitleData(item.node.top_offers_group).link}>
+                    {
+                        data.prismic.allHomes.edges?.map((item: any, i: any) => {
+                            return (
+                                <div key={i} className='textwithLink-group'>
+                                    <h1 className="title">{item.node.top_offers_title[i].text}</h1>
+                                    <div className='twl-item-wrap'>
+                                        {item.node.top_offers_group.map((item2: any, index1: any) => {
+                                            return (
+                                                <div className='content_wrap' key={"links_" + index1}>
+                                                    {item2.heading[i].text &&
+                                                        <div className='heading'>{item2.heading[i].text}</div>
+                                                    }
+                                                    {item2.sub_heading[i].text &&
+                                                        <div className='sub_heading'>{item2.sub_heading[i].text}</div>
+                                                    }
+                                                    {item2.view_offers_text[i].text &&
+                                                        <Link to={getTitleData(item.node.top_offers_group)[i].link}>
                                                             <div>{item2.view_offers_text[0].text}</div>
                                                         </Link>
-                                                        }
-                                                   </div>
-                                                  
-                                                  )
-                                                
-                                                })
-                                            }
-                                          
-                                        </div>
+                                                    }
+                                                </div>
+
+                                            )
+
+                                        })
+                                        }
+
                                     </div>
                                 </div>
-                            )
-                        })
+                                </div>
+            )
+        })
                     }
-                </div>
+                </div >
             )
 
         }}
-    />
+/>
 );
 
 export default TextWithLink;
